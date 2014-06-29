8bc27ea6eee574db10c7e67b12d2b6f8c6e2e34e
taabi <taabi@taabi-macbookpro.roam.corp.google.com>
2014-06-28 22:39:15 -0700

    Initial Commit

---
+++ FacebookLogin(week2)/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FacebookLogin(week2)
+//
+//  Created by taabi on 6/28/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FacebookLogin(week2)Tests/FacebookLogin_week2_Tests.m
@@ -0,0 +1,34 @@
+//
+//  FacebookLogin_week2_Tests.m
+//  FacebookLogin(week2)Tests
+//
+//  Created by taabi on 6/28/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FacebookLogin_week2_Tests : XCTestCase
+
+@end
+
+@implementation FacebookLogin_week2_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
